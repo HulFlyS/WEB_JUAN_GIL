b478a21e23b27d98e5a78c7c6c49919c81ceb90b
Juan Gil <juagilest@gmail.com>
2018-01-16 13:37:06 +0100

    HulFlyS

---
+++ codigo/layout.php
@@ -0,0 +1,93 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout CSS</title>
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/estilos.css" media="screen" title="no title">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="layout.css">
+  </head>
+  <body>
+    <div id="container">
+      <div id="cabecera">
+        <div class="img" id="c1">
+          <img src="cabecera.png">
+        </div>
+        <div id="c2">
+            <p1>SQUARE REGISTER</p1><br>
+            <p2>The POS software that's<br>
+            simple, powerful and free.</p2>
+            <button class="button">GET STARTED</button>
+        </div>
+        <div class="img" id="c3">
+          <img src="tabletsinfondo.png">
+        </div>
+      </div>
+    <div id="menus">
+      <div class="menu">
+        <div class="img">
+        <img src="img1.png"><br>
+        </div>
+        <div id="title1">
+        <b>Download free</b>
+        </div>
+        <div class="text">
+        <p>Square Register is the perfect point-
+          of-sale system, available in the App
+          Store of Google Play. It's designed
+          to run on a smartphone or tablet, on
+          yout counter or on the go.</p>
+        </div>
+      </div>
+      <div class="menu">
+        <div class="img">
+          <img src="img2.png">
+        </div>
+        <div id="title2">
+          <b>Grow your business</b>
+        </div>
+        <div class="text">
+          <p>Square Register is the point of sale
+          that takes care of digital receipts,
+          inventory, and sales reports and
+          provides valuable analytics and
+          feedback-so you can focus on your
+          customers.</p>
+        </div>
+      </div>
+      <div class="menu">
+        <div class="img">
+          <img src="img3.png"><br>
+        </div>
+        <div id="title3">
+        <b>Powerful enough for anyone</b>
+        </div>
+        <div class="text">
+        <p>Square Register server businesses
+          big and small, from corner bakeries
+          to retailers around the world. It
+          works with hardware and software
+          accesories that make it easier to
+          run your business.</p>
+        </div>
+      </div>
+      <div class="menu">
+        <div class="img">
+        <img src="img4.png"><br>
+        </div>
+        <div id="title4">
+        <b>Upgrades Automatically</b>
+        </div>
+        <div class="text">
+        <p>We're constanly improving with free
+          updates that equip you with great
+          new features to help you run your
+          business more efficiently and
+          effectively.</p>
+        </div>
+      </div>
+    </div>
+    </div>
+</body>

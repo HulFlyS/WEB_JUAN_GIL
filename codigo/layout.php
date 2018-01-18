66d4fd21e2aab5540a06749174ead412456fd92b
Juan Gil <juagilest@gmail.com>
2018-01-18 10:18:25 +0100

    HulFlyS

--- codigo/layout.php
+++ codigo/layout.php
@@ -4,90 +4,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Layout CSS</title>
     <title></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../css/estilos.css" media="screen" title="no title">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="layout.css">
   </head>
   <body>
-    <div id="container">
-      <div id="cabecera">
-        <div class="img" id="c1">
-          <img src="cabecera.png">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>RECETAS FÁCILES</h1>
         </div>
-        <div id="c2">
-            <p1>SQUARE REGISTER</p1><br>
-            <p2>The POS software that's<br>
-            simple, powerful and free.</p2>
-            <button class="button">GET STARTED</button>
+        <div class="col-md-2">
+          <a href="login.php" class=" btn btn-primary">Login</a>
         </div>
-        <div class="img" id="c3">
-          <img src="tabletsinfondo.png">
+        <div class="col-md-2">
+          <a href="regitro.php" class="btn btn-warning">Registro</a>
         </div>
       </div>
-    <div id="menus">
-      <div class="menu">
-        <div class="img">
-        <img src="img1.png"><br>
+      <div class"row">
+        <div class="col-md-6">
+          <a href="recetas.php" class="text-justify">Recetas</a>
         </div>
-        <div id="title1">
-        <b>Download free</b>
+        <div class="col-md-6">
+          <a href="contacto.php" class="text-justify">Contacto</a>
         </div>
-        <div class="text">
-        <p>Square Register is the perfect point-
-          of-sale system, available in the App
-          Store of Google Play. It's designed
-          to run on a smartphone or tablet, on
-          yout counter or on the go.</p>
-        </div>
-      </div>
-      <div class="menu">
-        <div class="img">
-          <img src="img2.png">
-        </div>
-        <div id="title2">
-          <b>Grow your business</b>
-        </div>
-        <div class="text">
-          <p>Square Register is the point of sale
-          that takes care of digital receipts,
-          inventory, and sales reports and
-          provides valuable analytics and
-          feedback-so you can focus on your
-          customers.</p>
-        </div>
-      </div>
-      <div class="menu">
-        <div class="img">
-          <img src="img3.png"><br>
-        </div>
-        <div id="title3">
-        <b>Powerful enough for anyone</b>
-        </div>
-        <div class="text">
-        <p>Square Register server businesses
-          big and small, from corner bakeries
-          to retailers around the world. It
-          works with hardware and software
-          accesories that make it easier to
-          run your business.</p>
-        </div>
-      </div>
-      <div class="menu">
-        <div class="img">
-        <img src="img4.png"><br>
-        </div>
-        <div id="title4">
-        <b>Upgrades Automatically</b>
-        </div>
-        <div class="text">
-        <p>We're constanly improving with free
-          updates that equip you with great
-          new features to help you run your
-          business more efficiently and
-          effectively.</p>
-        </div>
-      </div>
     </div>
-    </div>
+  </div>
 </body>
